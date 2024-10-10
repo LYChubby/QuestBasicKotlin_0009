b2a54402c47f60141b86cc379986b295d6f9070b
LYChubby <f4uzi345@gmail.com>
2024-10-11 00:24:52 +0700

    Membuat Perulangan

--- app/src/main/java/com/example/latihan1/ConditionalExpression.kt
+++ app/src/main/java/com/example/latihan1/ConditionalExpression.kt
@@ -30,3 +30,29 @@ fun ConditionalStatement() {
         println("Invalid Number")
     }
 }
+
+fun Perulangan() {
+    println()
+    println("=== PERULANGAN ===")
+
+    for (i in 1..5) {
+        println("Perulangan ke- $i")
+    }
+
+    val cars = listOf("BMW", "Toyota", "Honda", "Audi")
+    for (car in cars) {
+        println("car Type: $car")
+    }
+
+    var x = 1
+    while (x <= 5) {
+        println("Perulangan ke- $x")
+        x++
+    }
+
+    var y = 1
+    do {
+        println("Perulangan ke- $y")
+        y++
+    } while (y <= 5)
+}
