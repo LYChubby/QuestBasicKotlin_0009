ab326ef3080edf066cec01c5808f25610725341b
LYChubby <f4uzi345@gmail.com>
2024-10-11 00:11:12 +0700

    Membuat IF ELSE

---
+++ app/src/main/java/com/example/latihan1/ConditionalExpression.kt
@@ -0,0 +1,12 @@
+package com.example.latihan1
+
+fun ConditionalStatement() {
+    println("=== Conditional Expression ===")
+
+    val number = 10
+    if (number > 0) {
+        println("Positive Number")
+    } else {
+        println("Negative Number")
+    }
+}
