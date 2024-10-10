a448f80b95c1e6529bc4bce2c689d72857cbbaed
LYChubby <f4uzi345@gmail.com>
2024-10-10 23:33:55 +0700

    Membuat Class

--- app/src/main/java/com/example/latihan1/Class.kt
+++ app/src/main/java/com/example/latihan1/Class.kt
@@ -1,2 +1,15 @@
 package com.example.latihan1
 
+class Motor() {
+
+    class Contact(val id: Int, var email: String)
+}
+
+fun main() {
+    val contact = Motor.Contact(1, "mary@gmail.com")
+    println(contact.email)
+
+    contact.email = "john@gmail.com"
+    println(contact.email)
+
+}
