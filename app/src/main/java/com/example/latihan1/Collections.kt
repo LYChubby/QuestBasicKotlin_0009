6b8ba9d8184faa005d5e08177a4aad4edd52f951
LYChubby <f4uzi345@gmail.com>
2024-10-10 23:41:19 +0700

    Membuat Mutable List ( Dapat Diubah)

--- app/src/main/java/com/example/latihan1/Collections.kt
+++ app/src/main/java/com/example/latihan1/Collections.kt
@@ -4,4 +4,8 @@ fun ContohList() {
     println("==== LIST ====")
     val readOnlyAbjad = listOf("A", "B", "C")
     println(readOnlyAbjad)
+
+    val shape: MutableList<string> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
 }
+
